15be53941450644fc0f8e4fd12f458efb466ece6
kirill <kirill-esau@outlook.de>
2022-05-02 22:31:00 +0200

    Init

---
+++ src/main/java/de/kirill/minervaabsence/MinervaAbsenceApplication.java
@@ -0,0 +1,13 @@
+package de.kirill.minervaabsence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinervaAbsenceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MinervaAbsenceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/kirill/minervaabsence/MinervaAbsenceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kirill.minervaabsence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinervaAbsenceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
