d25210a4705aa810e3b605ed180e319f4e220438
kirill <kirill-esau@outlook.de>
2022-05-03 21:43:15 +0200

    Demo Aufruf

--- src/main/java/de/kirill/minervaabsence/MinervaAbsenceApplication.java
+++ src/main/java/de/kirill/minervaabsence/MinervaAbsenceApplication.java
@@ -2,9 +2,19 @@ package de.kirill.minervaabsence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("absence")
 public class MinervaAbsenceApplication {
+
+  @GetMapping("demo")
+  public String ping() {
+    return "Success";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(MinervaAbsenceApplication.class, args);
