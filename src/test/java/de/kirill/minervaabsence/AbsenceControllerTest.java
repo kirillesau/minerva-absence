e7ae5f1a35b4a4a917a240b63f26d92902de477d
kirill <kirill-esau@outlook.de>
2022-05-05 22:21:17 +0200

    Unauthorisierter User darf nicht auf den Endpoint zugreifen

---
+++ src/test/java/de/kirill/minervaabsence/AbsenceControllerTest.java
@@ -0,0 +1,32 @@
+package de.kirill.minervaabsence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.Matchers.containsString;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AbsenceControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void getAbsenceShouldReturn401WithoutAuthenticatedUser() throws Exception {
+    this.mockMvc.perform(get("/absence"))
+        .andDo(print())
+        .andExpect(status().isUnauthorized());
+  }
+
+}
