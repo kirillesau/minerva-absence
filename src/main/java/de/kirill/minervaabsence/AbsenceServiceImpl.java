81f2d9cc45a486c4400119bca341345d70951682
kirill <kirill-esau@outlook.de>
2022-05-13 15:03:21 +0200

    Service Annotation richtig gesetzt

--- src/main/java/de/kirill/minervaabsence/AbsenceServiceImpl.java
+++ src/main/java/de/kirill/minervaabsence/AbsenceServiceImpl.java
@@ -1,7 +1,10 @@
 package de.kirill.minervaabsence;
 
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
+@Service
 public class AbsenceServiceImpl implements AbsenceService {
 
   @Override
