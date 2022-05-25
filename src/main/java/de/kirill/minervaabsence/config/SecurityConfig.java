7bba6ab31c7d4e20f9851ffd2c1ec4c1eb67425c
kirill <kirill-esau@outlook.de>
2022-05-25 21:25:27 +0200

    Oauth2-Security-Konfiguration wird über ein Profil gesteuert

--- src/main/java/de/kirill/minervaabsence/config/SecurityConfig.java
+++ src/main/java/de/kirill/minervaabsence/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package de.kirill.minervaabsence.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
+@Profile("oauth2")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
